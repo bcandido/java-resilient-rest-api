e85baa5af1b98aba40516029d23e32727e3a3102
Brenno Candido <bcandido.bc@gmail.com>
2018-02-03 11:55:57 -0200

    Endpoint /about
    - Status checker of the service
    - Should always return 200 if the service is up

--- src/main/java/com/weather/controllers/About.java
+++ src/main/java/com/weather/controllers/About.java
@@ -1,14 +1,21 @@
 package com.weather.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @Controller
-@RequestMapping(path = "/about")
 public class About {
 
-    public About() {
-
+    @RequestMapping(path = "/about", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public HashMap<String, String> getAbout() {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "health");
+        return response;
     }
 
 }
